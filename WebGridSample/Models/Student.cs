5890f8b3b205a488162d63c4e27ef35bbcfbf7e4
tridip-bba <tridip@bba-reman.com>
2015-12-23 16:16:34 +0530

    update sort image issue

--- WebGridSample/Models/Student.cs
+++ WebGridSample/Models/Student.cs
@@ -9,38 +9,52 @@ namespace WebGridSample.Models
 {
     public class StudentVm
     {
-        public int page = 0, RowCount = 0;
-        public string sort = "", sortdir = "";
-        public int PageSize = 10;
+        public int page { get; set; }
+        public int RowCount { get; set; }
+        public int PageSize { get; set; }
+        public int CurrentPage { get; set; }
+
+        public string sort { get; set; }
+        public string sortdir { get; set; }
+
         public IList<Student> Students { get; set; }
 
+        public StudentVm()
+        {
+            PageSize = 5;
+            sort = "ID";
+            sortdir = "ASC";
+            CurrentPage = 1;
+        }
 
         public IList<Student> GetStudents(StudentVm oSVm)
         {
             int StartIndex = 0, EndIndex = 0;
 
             if (oSVm.page == 0)
-                page = 1;
+                oSVm.page = 1;
 
             StartIndex = ((oSVm.page * oSVm.PageSize) - oSVm.PageSize) + 1;
             EndIndex = (oSVm.page * oSVm.PageSize);
+            CurrentPage = StartIndex;
 
-            if (oSVm.sort == "")
+            if (string.IsNullOrEmpty(oSVm.sort))
                 oSVm.sort = "ID";
 
-            if (oSVm.sortdir == "")
+            if (string.IsNullOrEmpty(oSVm.sortdir))
                 oSVm.sortdir = "ASC";
 
             string connectionStringName = System.Configuration.ConfigurationManager.ConnectionStrings["StudentDBContext"].ConnectionString;
             IList<Student> _Student = new List<Student>();
 
-            string strSQL = "SELECT ID, FirstName,LastName,IsActive FROM Student WHERE ID >=" + StartIndex+ " AND ID <="+ EndIndex;
+            string strSQL = "SELECT ID, FirstName,LastName,IsActive,StateName,CityName FROM vwListStudents WHERE ID >=" + StartIndex + " AND ID <=" + EndIndex;
             strSQL += " ORDER BY " + oSVm.sort + " " + oSVm.sortdir;
 
+            strSQL += ";SELECT COUNT(*) AS Count FROM vwListStudents";
             using (SqlConnection connection = new SqlConnection(connectionStringName))
             {
                 SqlCommand command = new SqlCommand(
-                  ";", connection);
+                  strSQL, connection);
 
                 connection.Open();
 
@@ -55,13 +69,25 @@ namespace WebGridSample.Models
                             ID = Convert.ToInt32(reader["ID"].ToString()),
                             FirstName = reader["FirstName"].ToString(),
                             LastName = reader["LastName"].ToString(),
-                            IsActive = Convert.ToBoolean(reader["IsActive"])
+                            IsActive = Convert.ToBoolean(reader["IsActive"]),
+                            StateName = reader["StateName"].ToString(),
+                            CityName = reader["CityName"].ToString()
                         });
                     }
                 }
+
+                reader.NextResult();
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        RowCount = Convert.ToInt32(reader["Count"].ToString());
+                    }
+                }
+
                 reader.Close();
             }
-            RowCount = _Student.Count;
+            //RowCount = _Student.Count;
             return _Student;
         }
     }
@@ -72,5 +98,7 @@ namespace WebGridSample.Models
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public bool IsActive { get; set; }
+        public string StateName { get; set; }
+        public string CityName { get; set; }
     }
 }
